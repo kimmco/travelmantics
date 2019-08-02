26b01a3b0406da0c3cf1157360d70bee770bb7cc
cokimutai <cokimutai@gmail.com>
2019-08-02 20:11:05 +0300

    textView example

---
+++ app/src/main/java/com/cokimutai/travelmantics/InsertActivity.java
@@ -0,0 +1,31 @@
+    package com.cokimutai.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+    public class InsertActivity extends AppCompatActivity {
+
+        private FirebaseDatabase mFirebaseDatabase;
+        private DatabaseReference mDatabaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+
+    }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.save_menu, menu);
+            return true;
+        }
+    }
